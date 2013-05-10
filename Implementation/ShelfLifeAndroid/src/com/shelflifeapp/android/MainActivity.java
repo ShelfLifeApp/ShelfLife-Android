783285dcf4d4928fce28d80aeed47e37dfa98526
Marie Grap <mbgrap@gmail.com>
2013-05-09 18:00:59 -0700

    Implemented Bar Code Scanner

--- Implementation/ShelfLifeAndroid/src/com/shelflifeapp/android/MainActivity.java
+++ Implementation/ShelfLifeAndroid/src/com/shelflifeapp/android/MainActivity.java
@@ -1,10 +1,13 @@
 package com.shelflifeapp.android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
 
 public class MainActivity extends SherlockActivity {
 
@@ -16,7 +19,6 @@ public class MainActivity extends SherlockActivity {
         setContentView(R.layout.activity_main);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = this.getSupportMenuInflater();
@@ -25,4 +27,42 @@ public class MainActivity extends SherlockActivity {
 		return true;
     }
     
+    /**
+     * Called when an item in the Action Bar Sherlock Menu is pressed.
+     */
+    @Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle item selection
+	    switch (item.getItemId()) {
+	        case R.id.menu_ab_barcode:
+	        	IntentIntegrator.initiateScan(MainActivity.this);
+	            return true;
+	        case R.id.menu_ab_search:	        		           
+	        	return true;	        
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+    
+    /**
+     * Deals with the UPC code found by bar code scanner. 
+     */
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch(requestCode) {
+			case IntentIntegrator.REQUEST_CODE: {
+				if (resultCode != RESULT_CANCELED) {
+					IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+					if (scanResult != null) {
+						String upc = scanResult.getContents();
+			 
+						//put whatever you want to do with the code here
+						TextView tv = new TextView(this);
+						tv.setText(upc);
+						setContentView(tv);			 
+					}
+				}
+				break;
+			}
+		}
+	}
 }
